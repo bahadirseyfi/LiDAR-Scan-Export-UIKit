4ccb7d9434fb3374f4238c3123cd9720dba2222a
Bahadır Seyfi <bahadirsoftware@gmail.com>
2024-06-12 11:53:39 +0200

    Initial commit

--- Lidar-poc-uikit/ViewController.swift
+++ Lidar-poc-uikit/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import Metal
+import MetalKit
+import ARKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction
+    private func navigateButtonActiom(_ sender: UIButton) {
+        let storyboard : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc : PointCloudViewController = storyboard.instantiateViewController(withIdentifier: "Main") as! PointCloudViewController
 
+        let navigationController = UINavigationController(rootViewController: vc)
+
+        self.present(navigationController, animated: true, completion: nil)
+    }
+}
+
+// MARK: - RenderDestinationProvider
+protocol RenderDestinationProvider {
+    var currentRenderPassDescriptor: MTLRenderPassDescriptor? { get }
+    var currentDrawable: CAMetalDrawable? { get }
+    var colorPixelFormat: MTLPixelFormat { get set }
+    var depthStencilPixelFormat: MTLPixelFormat { get set }
+    var sampleCount: Int { get set }
+}
+
+extension SCNNode {
+    func cleanup() {
+        for child in childNodes {
+            child.cleanup()
+        }
+        self.geometry = nil
+    }
+}
+
+extension MTKView: RenderDestinationProvider {
+    
 }
 
